24c62b71590b34d9e7978761c2d7096c908d7fca
DevGuan <975081801@qq.com>
2017-05-17 18:08:01 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = '6969';
+
+// console.log(PORT)
+
+net.createServer(function(sock){
+    // 我们获得一个连接 - 该连接自动关联一个socket对象  
+    console.log('CONNECTED: ' +  
+        sock.remoteAddress + ':' + sock.remotePort);  
+        sock.write('服务端发出：连接成功');  
+
+    // 为这个socket实例添加一个"data"事件处理函数  
+    sock.on('data', function(data) {  
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);  
+        // 回发该数据，客户端将收到来自服务端的数据  
+        sock.write('You said "' + data + '"');  
+    });  
+    // 为这个socket实例添加一个"close"事件处理函数  
+    sock.on('close', function(data) {  
+        console.log('CLOSED: ' +  
+        sock.remoteAddress + ' ' + sock.remotePort);  
+    });  
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
